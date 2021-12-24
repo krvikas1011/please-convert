96ef15f1568f9f83db9d01d5a16bc47fef07db06
Vikas.Kumar <vikas.kumar@target.com>
2021-12-24 23:02:09 +0530

    removed ConversionCategory

--- src/main/java/com/pleaseconvert/model/Conversion.java
+++ src/main/java/com/pleaseconvert/model/Conversion.java
@@ -1,7 +1,5 @@
 package com.pleaseconvert.model;
 
 public interface Conversion {
-  String getName();
-
   String convert(String input);
 }
--- src/main/java/com/pleaseconvert/model/ConversionCategory.java
+++
@@ -1,9 +0,0 @@
-package com.pleaseconvert.model;
-
-import java.util.List;
-
-public interface ConversionCategory {
-  String getName();
-
-  List<Conversion> getConversions();
-}
