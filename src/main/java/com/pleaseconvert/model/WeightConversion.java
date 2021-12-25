ead3f29d3bc122237a6f3a3ca2173b3b052a229e
Vikas.Kumar <vikas.kumar@target.com>
2021-12-25 09:07:59 +0530

    added find by name for all enums

--- src/main/java/com/pleaseconvert/model/WeightConversion.java
+++ src/main/java/com/pleaseconvert/model/WeightConversion.java
@@ -1,16 +1,25 @@
 package com.pleaseconvert.model;
 
+import java.util.Arrays;
+
 public enum WeightConversion {
-    LBS_TO_KGS("LBS_TO_KGS"),
-    KGS_TO_LBS("KGS_TO_LBS");
+  LBS_TO_KGS("LBS_TO_KGS"),
+  KGS_TO_LBS("KGS_TO_LBS");
 
-    private final String name;
+  private final String name;
 
-    WeightConversion(String name) {
-        this.name = name;
-    }
+  WeightConversion(String name) {
+    this.name = name;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
+
+  public static WeightConversion findByName(String name) {
+    return Arrays.stream(WeightConversion.values())
+        .filter(weightConversion -> weightConversion.getName().equalsIgnoreCase(name))
+        .findFirst()
+        .orElse(null);
+  }
 }
